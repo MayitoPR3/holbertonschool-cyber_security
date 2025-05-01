abf8fd820493d30a6bf6b302c75b0810252a2573
Jose Mayo <josemayo3@gmail.com>
2025-04-30 21:00:41 -0400

    push task 1

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl curl http://10.42.239.138/reset_password NEW_HOST="$1" TARGET_URL="$2" FORM_DATA="$3"
+curl curl http://10.42.239.138/reset_password -X NEW_HOST="$1" TARGET_URL="$2" FORM_DATA="$3"
