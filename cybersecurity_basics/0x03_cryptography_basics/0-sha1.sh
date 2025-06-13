d4359df8a10a37635c975dc83fb81c200129e384
Jose Mayo <josemayo3@gmail.com>
2025-06-13 10:56:15 -0400

    push task

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if a password argument was provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <password>"
-  exit 1
-fi
-
-# Hash the password using SHA-1 and save to 0_hash.txt
+[ -z "$1" ] && { echo "Usage: $0 <password>"; exit 1; }
 echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
