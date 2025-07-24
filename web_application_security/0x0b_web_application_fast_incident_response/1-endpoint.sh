a1bdf2369756b1cc07cd4bcc9f2d97ed6ac2d740
Jose Mayo <josemayo3@gmail.com>
2025-07-24 11:42:58 -0400

    push tasks

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Bash script to find the endpoint (URL) that received the most requests, indicating it was likely the target of the attack.
-awk -F'"' '{print $2}' logs.txt | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+awk -F'"' '{print $2}' logs.txt | awk '{print $2}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
