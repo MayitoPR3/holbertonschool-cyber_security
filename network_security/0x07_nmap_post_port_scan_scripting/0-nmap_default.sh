9d5c7cf017afe361c72ad87736db03815e7e4a05
Jose Mayo <josemayo3@gmail.com>
2025-08-01 16:19:09 -0400

    push task 0

--- network_security/0x07_nmap_post_port_scan_scripting/0-nmap_default.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/0-nmap_default.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Check if target is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <target-host>"
-    exit 1
-fi
-
 TARGET="$1"
-
-echo "[+] Running Nmap default script scan on: $TARGET"
-
-# Run nmap with default scripts (-sC), service/version detection (-sV), and show reason (-reason)
 sudo nmap -sC -sV "$TARGET"
