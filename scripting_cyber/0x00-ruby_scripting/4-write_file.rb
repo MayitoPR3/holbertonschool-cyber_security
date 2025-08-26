0de0c3106ca8c57b9ea430f00cdd6707ca4c0848
Jose Mayo <josemayo3@gmail.com>
2025-08-26 01:02:59 -0400

    push task

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -12,5 +12,8 @@ def merge_json_files(file1_path, file2_path)
   # Merge arrays
   merged_data = data2 + data1
 
+  # Write back into file2_path (overwrite with merged data)
+  File.open(file2_path, "w") do |f|
+    f.write(JSON.pretty_generate(merged_data))
   end
 end
