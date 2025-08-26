c0ef398683153ea57b1edb33306adc2e09abf854
Jose Mayo <josemayo3@gmail.com>
2025-08-26 18:29:48 -0400

    push task

--- scripting_cyber/0x00-ruby_scripting/5-cipher.rb
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -15,14 +15,13 @@ class CaesarCipher
 
   def cipher(message, shift)
     message.chars.map do |char|
-        if char =~ /[A-Z]/
-            (((char.ord - 'A'.ord + shift) % 26) + 'A'.ord).chr
-        elsif char =~ /[a-z]/
-            (((char.ord - 'a'.ord +shift) % 26) + 'a'.ord).chr
-        else
-            char
-        end
-      end.join
-    end
+      if char =~ /[A-Z]/   # uppercase
+        (((char.ord - 'A'.ord + shift) % 26) + 'A'.ord).chr
+      elsif char =~ /[a-z]/ # lowercase
+        (((char.ord - 'a'.ord + shift) % 26) + 'a'.ord).chr
+      else
+        char # punctuation, spaces, digits unchanged
+      end
+    end.join
+  end
 end
-
