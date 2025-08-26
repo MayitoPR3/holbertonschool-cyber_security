3046dfac6cda1e5650aeedebb420b79541187c49
Jose Mayo <josemayo3@gmail.com>
2025-08-26 00:46:29 -0400

    push task

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,20 @@
+require 'prime'
+
+def count_user_ids(path)
+    # Read file content
+    file_content = File.read(path)
+    
+    # Parse JSON into Ruby objects (usually an array of hashes)
+    data = JSON.parse(file_content)
+
+    # Count occurrences of each userId
+    counts = Hash.new(0)
+    data.each do |item|
+        counts[item["userID"]] += 1
+    end
+    
+    # Print results in sorted order by userId
+    counts.sort.each do |user_id, count|
+        puts "#{user_id}: #{count}"
+    end
+end
