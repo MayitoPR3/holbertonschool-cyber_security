4eb7d23db494536080b5a34252af8d989ad8a796
Jose Mayo <josemayo3@gmail.com>
2025-08-26 01:01:42 -0400

    push task

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,11 +1,14 @@
-require '3-read_file'
-require '4-write_file'
+require 'json'
 
-file1_path = 'file2.json'
-file2_path = 'file.json'
+def merge_json_files(file1_path, file2_path)
+  # Read both files
+  file1_content = File.read(file1_path)
+  file2_content = File.read(file2_path)
 
-merge_json_files(file1_path, file2_path)
+  # Parse JSON arrays
+  data1 = JSON.parse(file1_content)
+  data2 = JSON.parse(file2_content)
 
-puts 'File merged successfully!'
-
-count_user_ids(file2_path)
+  # Merge arrays
+  merged_data = data2 + data1
+  
