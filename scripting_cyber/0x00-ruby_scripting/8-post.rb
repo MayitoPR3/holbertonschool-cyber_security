79e80a59b0bd8307bdb4e6b1ff1d0240ee28eb92
Jose Mayo <josemayo3@gmail.com>
2025-08-26 19:26:29 -0400

    push task

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,14 @@
+require 'uri'
+require 'net/http'
+
+
+def post_request(url, body_params)
+    uri = URI(url)
+    res = Net::HTTP.post_form(uri, body_params)
+
+    puts "Response status: #{res.code} #{res.message}"
+
+    puts "Response body:"
+    puts res.body
+end
+
