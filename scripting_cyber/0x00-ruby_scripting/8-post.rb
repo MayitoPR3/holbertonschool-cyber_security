aaffb16c644f37893ac998a95ef72246ba7b8a76
Jose Mayo <josemayo3@gmail.com>
2025-08-26 19:46:23 -0400

    push task

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -2,21 +2,24 @@ require 'net/http'
 require 'json'
 require 'uri'
 
-def post_request(url)
+def post_request(url, body_params = {})
   uri = URI(url)
-  response = Net::HTTP.get_response(uri)
+
+  # Create HTTP request with headers
+  headers = { 'Content-Type' => 'application/json' }
+  response = Net::HTTP.post(uri, body_params.to_json, headers)
 
   # Print response status
   puts "Response status: #{response.code} #{response.message}"
 
-  # Parse body as JSON if possible, otherwise print raw
+  # Try parsing as JSON
   begin
     json_body = JSON.parse(response.body)
     puts "Response body:"
     if json_body.empty?
-      puts JSON.generate(json_body) # prints {}
+        puts JSON.generate(json_body)
     else
-      puts JSON.pretty_generate(json_body)
+        puts JSON.pretty_generate(json_body)
     end
   rescue JSON::ParserError
     puts "Response body:"
