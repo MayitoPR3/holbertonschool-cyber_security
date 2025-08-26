5a5dd1ba3eb289ab83ae7cd1d65f89fab3e85f94
Jose Mayo <josemayo3@gmail.com>
2025-08-26 00:59:08 -0400

    push task

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,19 +1,11 @@
-require 'json'
+require_relative '3-read_file'
+require_relative '4-write_file'
 
-def merge_json_files(file1_path, file2_path)
-  # Read both files
-  file1_content = File.read(file1_path)
-  file2_content = File.read(file2_path)
+file1_path = 'file2.json'
+file2_path = 'file.json'
 
-  # Parse JSON arrays
-  data1 = JSON.parse(file1_content)
-  data2 = JSON.parse(file2_content)
+merge_json_files(file1_path, file2_path)
 
-  # Merge arrays
-  merged_data = data2 + data1
+puts 'File merged successfully!'
 
-  # Write back into file2_path (overwrite with merged data)
-  File.open(file2_path, "w") do |f|
-    f.write(JSON.pretty_generate(merged_data))
-  end
-end
+count_user_ids(file2_path)
