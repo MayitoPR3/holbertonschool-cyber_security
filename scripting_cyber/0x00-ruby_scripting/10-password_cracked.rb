2426c26f83af5c0a169e8127d931c7d4d23fce97
Jose Mayo <josemayo3@gmail.com>
2025-08-27 01:26:19 -0400

    push task

---
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -0,0 +1,31 @@
+require 'digest'
+
+# Check arguments
+if ARGV.length != 2
+    puts "Usage: #{$0} HASH_PASSWORD DICTIONARY_FILE"
+    exit
+end
+
+hash_password = ARGV[0]
+dictionary_file = ARGV[1]
+
+begin
+    found = false
+    File.foreach(dictionary_file) do |word|
+        word.strip!
+        next if word.empty?
+
+        # Compute SHA-256 hash
+        hash = Digest::SHA256.hexdigest(word)
+
+        if hasg == hashed_password
+            puts "Password found: #{word}"
+            found = true
+            break
+        end
+    end
+
+    puts "Password not found in dictionary." unless found
+rescue Errno::ENOENT
+    puts "Error: Dictionary file not found."
+end
